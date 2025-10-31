43e232ca48cf34fb7f44bd2e85b0c0c4cf803431
liviabontempol <liviabontempolivia@gmail.com>
2025-10-31 10:23:26 -0300

    feature: adicionando server

---
+++ server/src/server.js
@@ -0,0 +1,43 @@
+    import express from "express";
+    //import bodyParser from "body-parser";
+    import fs from "fs";
+    import path from "path";
+        import cors from "cors";
+        import { gerarPlanilha } from "./utils/gerarPlanilha.js";
+
+
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    //app.use(helmelt());
+    //app.use(bodyParser.json());   
+    //app.use(express.static("public"));  serve o front, ver se faz sentido ter essa linha
+
+
+    app.post("/gerar-planilha", async (req, res) => {
+    try {
+        const dados = req.body; 
+        const caminhoArquivo = await gerarPlanilha(dados);  
+    
+     res.download(caminhoArquivo, path.basename(caminhoArquivo), (err) => {
+      if (err) {
+        console.error('Erro no download:', err);
+        if (!res.headersSent) res.status(500).send('Erro ao enviar arquivo');
+      }
+      // remove assincronamente 
+      fs.unlink(caminhoArquivo, (unlinkErr) => {
+        if (unlinkErr) console.error('Erro ao remover tmp:', unlinkErr);
+      });
+    });
+
+
+    } catch (error) {
+        console.error( error);
+       // res.status(500).send("Erro ao gerar planilha");
+        if (!res.headersSent) res.status(500).send('Erro ao gerar planilha');
+
+    }
+    });
+        
+    const PORT = 3000;
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
---
+++ server/src/utils/gerarPlanilha.js
@@ -0,0 +1,5 @@
+function gerarPlanilha(dados) {
+  console.log(dados, `Aplicacao deu green! Dados: ${dados}`);
+}
+
+export {gerarPlanilha };
